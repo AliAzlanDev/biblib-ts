5d01c141196ceda9c90a1edfd27d54cc663d8d74
Ali Azlan <136848358+aliazlanpro@users.noreply.github.com>
2025-03-08 04:15:24 +0500

    bump version to 0.1.3 and export BibLibRef type

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { parseMedline } from './medline.js';
 export { parseEndnoteXML } from './endnote-xml.js';
 export { parseBibTeX } from './bibtex.js';
 export { parseCSV } from './csv.js';
+export type { BibLibRef } from './types/index.js';
